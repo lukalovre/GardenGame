746a5e8d0369188e675fb9f23b28ef838f828559
Luka Lovre <lukalovre92@gmail.com>
2021-01-08 14:12:35 +0100

    Aded tilemap

---
+++ Assets/Code/GenerateMap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMap : MonoBehaviour
+{
+	private void GenerateGrid(int rows, int columns)
+	{
+		var tile = Instantiate(Resources.Load("GrassTile"));
+
+		for(int y = 0; y < columns; y++)
+		{
+			for(int x = 0; x < rows; x++)
+			{
+			}
+		}
+	}
+
+	// Start is called before the first frame update
+	private void Start()
+	{
+		GenerateGrid(10, 5);
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+	}
+}
