fa179c80328eeb5f6259d80b9c71ad4d57026a65
Luka Lovre <lukalovre92@gmail.com>
2021-01-08 21:13:40 +0100

    Small rename

--- Assets/Code/GenerateMap.cs
+++ Assets/Code/GenerateMap.cs
@@ -7,13 +7,13 @@ public class GenerateMap : MonoBehaviour
 	public TileBase rock;
 	public Tilemap tilemap;
 
-	private void GenerateGrid(int rows, int columns)
+	private void GenerateGrid(int columns, int rows)
 	{
 		tilemap.ClearAllTiles();
 
-		for(int y = 0; y < columns; y++)
+		for(int y = 0; y < rows; y++)
 		{
-			for(int x = 0; x < rows; x++)
+			for(int x = 0; x < columns; x++)
 			{
 				var tile = rock;
 
