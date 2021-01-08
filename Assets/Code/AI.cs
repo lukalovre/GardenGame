6142d015bd89c65a5942b46e148f08c227a7a36b
Luka Lovre <lukalovre92@gmail.com>
2021-01-08 22:42:42 +0100

    Ant moves

---
+++ Assets/Code/AI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+	// Start is called before the first frame update
+	private void Start()
+	{
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		transform.position = Vector2.MoveTowards(transform.position, new Vector2(0, 0), 1 * Time.deltaTime);
+	}
+}
