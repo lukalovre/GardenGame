d87165420c115da9b5201c4626f9f6c5afb9d321
Luka Lovre <lukalovre92@gmail.com>
2021-01-09 15:44:38 +0100

    Random moving on turn

--- Assets/Code/AI.cs
+++ Assets/Code/AI.cs
@@ -5,8 +5,36 @@ public class AI : MonoBehaviour
 {
 	public static bool DoTurn;
 	private Collider2D collider;
-	private Vector2Int NextLocation;
-	private List<Vector2Int> Path = new List<Vector2Int>();
+	private Vector3? NextLocation;
+	private List<Vector3> Path = new List<Vector3>();
+
+	private static Vector3 GetRandomDirection()
+	{
+		var randomDirection = Random.Range(1, 5);
+		var randomDirectionVector = new Vector3(0, 0);
+
+		if(randomDirection == 1)
+		{
+			randomDirectionVector = new Vector3(-1, 0);
+		}
+
+		if(randomDirection == 2)
+		{
+			randomDirectionVector = new Vector3(0, 1);
+		}
+
+		if(randomDirection == 3)
+		{
+			randomDirectionVector = new Vector3(1, 0);
+		}
+
+		if(randomDirection == 4)
+		{
+			randomDirectionVector = new Vector3(0, -1);
+		}
+
+		return randomDirectionVector;
+	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
@@ -26,6 +54,21 @@ public class AI : MonoBehaviour
 			return;
 		}
 
-		transform.position = Vector2.MoveTowards(transform.position, new Vector2(0, 0), 1 * Time.deltaTime);
+		if(NextLocation == null)
+		{
+			NextLocation = transform.position + GetRandomDirection();
+		}
+
+		if(transform.position == NextLocation)
+		{
+			NextLocation = null;
+			DoTurn = false;
+		}
+		else
+		{
+			transform.position = Vector2.MoveTowards(transform.position,
+				NextLocation.Value,
+				1 * Time.deltaTime);
+		}
 	}
 }
