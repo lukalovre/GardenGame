56f7dd52633a14f770073046e10c83a3c22866e7
Luka Lovre <lukalovre92@gmail.com>
2021-01-09 11:23:35 +0100

    Generating rock as objects on map

--- Assets/Code/GenerateMap.cs
+++ Assets/Code/GenerateMap.cs
@@ -1,26 +1,44 @@
-﻿using UnityEngine;
-using UnityEngine.Tilemaps;
+﻿using Assets.Code;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class GenerateMap : MonoBehaviour
 {
-	public TileBase rock;
-	public Tilemap tilemap;
+	private List<GridObject> m_gridObjectList;
 
 	private void GenerateGrid(int columns, int rows)
 	{
-		tilemap.ClearAllTiles();
+		m_gridObjectList = new List<GridObject>(columns * rows);
 
 		for(int y = 0; y < rows; y++)
 		{
 			for(int x = 0; x < columns; x++)
 			{
-				var tile = rock;
+				GridObject gridObject;
 
-				if(Random.Range(1, 7) == 1)
+				if(Random.Range(1, 5) == 1)
 				{
-					tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+					gridObject = new GridObject
+					{
+						ObjectType = GridObject.Type.Rock,
+						Position = new Vector2Int(x, y)
+					};
 				}
+				else
+				{
+					gridObject = new GridObject
+					{
+						Position = new Vector2Int(x, y)
+					};
+				}
+
+				m_gridObjectList.Add(gridObject);
 			}
+		}
+
+		foreach(var gridObject1 in m_gridObjectList)
+		{
+			gridObject1.Create();
 		}
 	}
 
---
+++ Assets/Code/GridObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Assets.Code
+{
+	public class GridObject
+	{
+		public Type ObjectType;
+		public Vector2Int Position;
+
+		public enum Type
+		{
+			Empty,
+			Rock
+		}
+
+		public void Create()
+		{
+			if(ObjectType == Type.Rock)
+			{
+				var someGameObject = GameObject.Find("Rock");
+				var go = Object.Instantiate(someGameObject);
+				go.transform.position = (Vector2)Position;
+			}
+		}
+	}
+}
