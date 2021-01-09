0e9e7b55dfd572e98bd2378f296b63cd18e87b03
Luka Lovre <lukalovre92@gmail.com>
2021-01-09 15:16:58 +0100

    Added card class

---
+++ Assets/Code/Card.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+	private Collider2D collider;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+	}
+
+	// Start is called before the first frame update
+	private void Start()
+	{
+		collider = GetComponent<Collider2D>();
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		if(Input.touchCount == 0)
+		{
+			return;
+		}
+
+		var touch = Input.GetTouch(0);
+		var touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+		var touchedCollider = Physics2D.OverlapPoint(touchPosition);
+
+		if(collider == touchedCollider)
+		{
+			transform.position = Vector2.MoveTowards(transform.position, new Vector2(0, 0), 1 * Time.deltaTime);
+		}
+	}
+}
