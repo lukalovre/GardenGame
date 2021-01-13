3ea6ef874bc53554229d6aae8933d14e00bbe571
Luka Lovre <lukalovre92@gmail.com>
2021-01-13 21:26:43 +0100

    Ref

--- Assets/Code/Button.cs
+++ Assets/Code/Button.cs
@@ -1,5 +1,4 @@
-﻿using Assets.Code;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Button : MonoBehaviour
@@ -7,7 +6,6 @@ public class Button : MonoBehaviour
 	public string NextScene;
 	public bool ToggleButton;
 	public bool ToggleValue = true;
-	private Collider2D m_collider;
 
 	private void ButtonClick()
 	{
@@ -30,18 +28,5 @@ public class Button : MonoBehaviour
 	private void OnMouseDown()
 	{
 		ButtonClick();
-	}
-
-	private void Start()
-	{
-		m_collider = GetComponent<Collider2D>();
-	}
-
-	private void Update()
-	{
-		if(TouchInput.IsTouched(m_collider))
-		{
-			ButtonClick();
-		}
 	}
 }
