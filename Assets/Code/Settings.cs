4f715d1ad6a53787d275aa642043350d54b283d9
Luka Lovre <lukalovre92@gmail.com>
2021-01-13 22:53:26 +0100

    Saved all setting values adsn made the series work

--- Assets/Code/Settings.cs
+++ Assets/Code/Settings.cs
@@ -8,21 +8,45 @@ public class Settings : MonoBehaviour
 	public bool Series;
 	public bool ToggleButton;
 	public int Value;
-	public int Height { get; private set; } = GlobalSettings.MAX_HEIGHT;
+
 	public bool ToggleValue { get; private set; }
-	public int Width { get; private set; } = GlobalSettings.MAX_WIDTH;
 
 	private void ButtonClick()
 	{
+		if(Series)
+		{
+			SelectValue(Value);
+			return;
+		}
+
 		if(ToggleButton)
 		{
-			ToggleValue = !ToggleValue;
-			GetComponent<Animator>().enabled = ToggleValue;
-			GetComponent<SpriteRenderer>().color = ToggleValue ? Color.white : Color.black;
+			HandleToggleButton();
 			return;
 		}
 
 		LoadNextScene();
+	}
+
+	private void ChangeButtonStatus(bool freeze)
+	{
+		GetComponent<Animator>().enabled = !freeze;
+		GetComponent<SpriteRenderer>().color = freeze ? Color.black : Color.white;
+	}
+
+	private void HandleToggleButton()
+	{
+		if(gameObject.name == "Music")
+		{
+			GlobalSettings.MusicOn = ToggleValue;
+		}
+		else
+		{
+			GlobalSettings.SoundOn = ToggleValue;
+		}
+
+		ToggleValue = !ToggleValue;
+		ChangeButtonStatus(ToggleValue);
 	}
 
 	private void LoadNextScene()
@@ -33,5 +57,41 @@ public class Settings : MonoBehaviour
 	private void OnMouseDown()
 	{
 		ButtonClick();
+	}
+
+	private void SelectValue(int value)
+	{
+		if(gameObject.CompareTag("Width"))
+		{
+			GlobalSettings.Width = value;
+		}
+		else
+		{
+			GlobalSettings.Height = value;
+		}
+
+		foreach(var gameObject in GameObject.FindGameObjectsWithTag(gameObject.tag))
+		{
+			var gameObjectValue = gameObject.GetComponent<Settings>().Value;
+
+			gameObject.GetComponent<Settings>().ChangeButtonStatus(gameObjectValue != value);
+		}
+	}
+
+	private void Start()
+	{
+		if(Series)
+		{
+			if(gameObject.CompareTag("Width"))
+			{
+				SelectValue(GlobalSettings.MAX_WIDTH);
+			}
+			else
+			{
+				SelectValue(GlobalSettings.MAX_HEIGHT);
+			}
+
+			return;
+		}
 	}
 }
